78e05f630e21325fbb03e930f58054d0db00517a
Serci Serge <serge.serci@hotmail.fr>
2022-06-14 14:41:13 +0200

    fix relations

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,5 +1,5 @@
 class Objective < ApplicationRecord
-  belongs_to :user, through: :pots
+  belongs_to :user
   validates :title, :amount, :reaching_date, presence: true
   validates :amount, numericality: { only_integer: true, greater_than: 0 }
   validates :title, length: { maximum: 20 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :consumptions
   has_many :pots
+  has_many :objectives, through: :pots
   validates :email, :first_name, :last_name, presence: true
   validates :iban, format: {with: /FR\d{12}[A-Z0-9]{11}\d{2}/ }
   validates :age, numericality: { only_integer: true, greater_than: 10 }
