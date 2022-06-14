74139f70d8e9298664e8e8bc02fc9805df321e54
Serci Serge <serge.serci@hotmail.fr>
2022-06-14 17:54:39 +0200

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,25 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :consumptions, only: [:new, :create] do
+    resources :cigarettes, only: [:create]
+  end
+  get '/login', to: 'pages#login', as: 'login'
+  get '/simulation', to: 'pages#simulation', as: 'simulation'
+  get '/result', to: 'pages#result', as: 'result'
+  get '/objective/new', to: 'pages#new_objective', as: 'new_objective'
+  post '/objective/create', to: 'pages#create_objective', as: 'create_objective'
+  get '/objective/show', to: 'pages#show_objective', as: 'show_objective'
+  get '/dashboard', to: 'dashboards#dashboard', as: 'dashboard'
+  get '/dashboard/pot', to: 'dashboards#pot', as: 'dashboard_pot'
+  get '/dashboard/health', to: 'dashboards#health', as: 'dashboard_health'
+  get '/dashboard/motivation', to: 'dashboards#motivation', as: 'dashboard_motivation'
+  get '/dashboard/consumption', to: 'dashboards#new_consumption', as: 'dashboard_new_consumption'
+  post '/dashboard/consumption', to: 'dashboards#create_consumption', as: 'dashboard_create_consumption'
+
+
+
+
+
+
 end
