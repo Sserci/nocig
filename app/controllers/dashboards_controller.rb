d58384f077b1c4de6489ea5978d2b48461b79ff8
Gregory Bert <gregory_bert@yahoo.com>
2022-06-15 17:34:55 +0200

    page cagnotte en bonne voie

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,8 @@
 class DashboardsController < ApplicationController
+  def pot
+    # @pot = Pot.new
+  end
+
+  def objective_amount
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :consumptions
+  # has_many :consumptions
 
-  validates :email, :first_name, :last_name, presence: true
-  validates :iban, format: {with: /FR\d{12}[A-Z0-9]{11}\d{2}/ }
-  validates :age, numericality: { only_integer: true, greater_than: 10 }
-  validates :gender, inclusion: { in: %w(Femme Homme Autre)}
+  # validates :email, :first_name, :last_name, presence: true
+  # validates :iban, format: {with: /FR\d{12}[A-Z0-9]{11}\d{2}/ }
+  # validates :age, numericality: { only_integer: true, greater_than: 10 }
+  # validates :gender, inclusion: { in: %w(Femme Homme Autre)}
 
-  # objectives validate
-  validates :objective_amount, numericality: { only_integer: true, greater_than: 0 }
-  validates :objective_title, length: { maximum: 20 }
+  # # objectives validate
+  # validates :objective_amount, numericality: { only_integer: true, greater_than: 0 }
+  # validates :objective_title, length: { maximum: 20 }
 
-  # pots validates
-  validates :pot, numericality: { only_integer: true, greater_than: 0 }
+  # # pots validates
+  # validates :pot, numericality: { only_integer: true, greater_than: 0 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
