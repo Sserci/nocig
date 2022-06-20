3d1d5583b5692f01c7e8f01c512970d8322575fc
Serci Serge <serge.serci@hotmail.fr>
2022-06-20 10:04:54 +0200

    add pwa

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # ngrok
+  config.hosts << /[a-z0-9\-]+\.ngrok\.io/
+  config.hosts << /[a-z0-9-]+\.ngrok\.io/
+  config.hosts << "f198-85-69-174-28.eu.ngrok.io"
 end
