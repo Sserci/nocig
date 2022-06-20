d3643690bdd5abe70f0d58357e0b5aedb7480d20
Gregory Bert <gregory_bert@yahoo.com>
2022-06-20 18:01:36 +0200

    line chart tested and working

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
   def pot
     @all_transactions = {}
     current_user.transactions.each do |t|
-      @all_transactions["t.date"] = t.packs_number
+      @all_transactions[t.date] = t.amount
     end
   end
 
